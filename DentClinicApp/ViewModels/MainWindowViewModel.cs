774fcbf83aa34d1be9a8e57ebde82e1a8ad3f84a
Anna Iwanek-Sikorska <aniaiwa4@gmail.com>
2024-11-27 18:08:12 +0100

    modyfikacje metod showall i create, dodanie typu generycznego, zmniejszenie ilości powtarzalego kodu

--- DentClinicApp/ViewModels/MainWindowViewModel.cs
+++ DentClinicApp/ViewModels/MainWindowViewModel.cs
@@ -1,133 +1,60 @@
 ﻿using DentClinicApp.Helper;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
-using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
+using System.Windows.Input;
 
 namespace DentClinicApp.ViewModels
 {
-    public class MainWindowViewModel : BaseViewModel // na podstawie wykładu 
+    public class MainWindowViewModel : BaseViewModel
     {
-        #region Fields
-        private ReadOnlyCollection<CommandViewModel> _Commands;
-        private ObservableCollection<WorkspaceViewModel> _Workspaces;
-        #endregion
+        // Komendy do obsługi przycisków w widoku
+        public ICommand ShowPacjenciCommand => new BaseCommand(() => ShowWorkspace<WszyscyPacjenciViewModel>());
+        public ICommand ShowPracownicyCommand => new BaseCommand(() => ShowWorkspace<WszyscyPracownicyViewModel>());
 
-        #region Commands
-        public ReadOnlyCollection<CommandViewModel> Commands
+        // Aktualny aktywny workspace (widok w centralnej części aplikacji)
+        private WorkspaceViewModel _activeWorkspace;
+        public WorkspaceViewModel ActiveWorkspace
         {
-            get
+            get => _activeWorkspace;
+            set
             {
-                if (_Commands == null)
+                if (_activeWorkspace != value)
                 {
-                    List<CommandViewModel> cmds = this.CreateCommands();
-                    _Commands = new ReadOnlyCollection<CommandViewModel>(cmds);
+                    _activeWorkspace = value;
+                    OnPropertyChanged(() => ActiveWorkspace);
                 }
-                return _Commands;
             }
         }
-        private List<CommandViewModel> CreateCommands()
+
+        // Kolekcja wszystkich otwartych widoków
+        public ObservableCollection<WorkspaceViewModel> Workspaces { get; }
+
+        // Konstruktor
+        public MainWindowViewModel()
         {
-            return new List<CommandViewModel>
-            {
-                new CommandViewModel(
-                    "Pacjenci",
-                    new BaseCommand(() => this.ShowAllPacjent())),
-
-                new CommandViewModel(
-                    "Pacjent",
-                    new BaseCommand(() => this.CreateView(new NowyPacjentViewModel()))),
-
-                new CommandViewModel(
-                    "Pracownik",
-                    new BaseCommand(() => this.ShowAllPracownik())),
-
-                new CommandViewModel(
-                    "Pracownicy",
-                    new BaseCommand(() => this.CreateView(new NowyPacjentViewModel())))
-
-            };
-        }
-        #endregion
-
-        #region Workspaces
-        public ObservableCollection<WorkspaceViewModel> Workspaces
-        {
-            get
-            {
-                if (_Workspaces == null)
-                {
-                    _Workspaces = new ObservableCollection<WorkspaceViewModel>();
-                    _Workspaces.CollectionChanged += this.OnWorkspacesChanged;
-                }
-                return _Workspaces;
-            }
-        }
-        private void OnWorkspacesChanged(object sender, NotifyCollectionChangedEventArgs e)
-        {
-            if (e.NewItems != null && e.NewItems.Count != 0)
-                foreach (WorkspaceViewModel workspace in e.NewItems)
-                    workspace.RequestClose += this.OnWorkspaceRequestClose;
-
-            if (e.OldItems != null && e.OldItems.Count != 0)
-                foreach (WorkspaceViewModel workspace in e.OldItems)
-                    workspace.RequestClose -= this.OnWorkspaceRequestClose;
-        }
-        private void OnWorkspaceRequestClose(object sender, EventArgs e)
-        {
-            WorkspaceViewModel workspace = sender as WorkspaceViewModel;
-            //workspace.Dispos();
-            this.Workspaces.Remove(workspace);
+            Workspaces = new ObservableCollection<WorkspaceViewModel>();
         }
 
-        #endregion // Workspaces
-
-        #region Private Helpers
-        private void CreateView(WorkspaceViewModel nowy)
+        // Generyczna metoda do wyświetlania widoku
+        private void ShowWorkspace<T>() where T : WorkspaceViewModel, new()
         {
-            this.Workspaces.Add(nowy); // dodanie zakładki do kolekcji zakładek 
-            this.SetActiveWorkspace(nowy); // aktywowanie zakładki 
-        }
-        private void ShowAllPacjent()
-        {
-            WszyscyPacjenciViewModel workspace =
-                this.Workspaces.FirstOrDefault(vm => vm is WszyscyPacjenciViewModel)
-                as WszyscyPacjenciViewModel;
+            // Sprawdzenie, czy workspace już istnieje
+            var workspace = Workspaces.OfType<T>().FirstOrDefault();
             if (workspace == null)
             {
-                workspace = new WszyscyPacjenciViewModel();
-                this.Workspaces.Add(workspace);
+                // Jeśli nie istnieje, utwórz nowy
+                workspace = new T();
+                Workspaces.Add(workspace);
             }
-
-            this.SetActiveWorkspace(workspace);
+            // Ustaw jako aktywny widok
+            SetActiveWorkspace(workspace);
         }
-        private void ShowAllPracownik()
-        {
-            WszyscyPracownicyViewModel workspace =
-                this.Workspaces.FirstOrDefault(vm => vm is WszyscyPracownicyViewModel)
-                as WszyscyPracownicyViewModel;
-            if (workspace == null)
-            {
-                workspace = new WszyscyPracownicyViewModel();
-                this.Workspaces.Add(workspace);
-            }
 
-            this.SetActiveWorkspace(workspace);
-        }
+        // Ustawienie aktywnego workspace
         private void SetActiveWorkspace(WorkspaceViewModel workspace)
         {
-            Debug.Assert(this.Workspaces.Contains(workspace));
-
-            ICollectionView collectionView = CollectionViewSource.GetDefaultView(this.Workspaces);
-            if (collectionView != null)
-                collectionView.MoveCurrentTo(workspace);
+            ActiveWorkspace = workspace;
         }
-        #endregion
     }
 }
